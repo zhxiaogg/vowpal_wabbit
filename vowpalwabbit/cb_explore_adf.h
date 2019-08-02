bd6538af316c82f1bcd6ee49bac0b6561a2d768e
Jack Gerrits <jackgerrits95@gmail.com>
2019-08-02 13:45:25 -0400

    Revert usages of multi_ex to v_array

--- vowpalwabbit/cb_explore_adf.h
+++ vowpalwabbit/cb_explore_adf.h
@@ -12,5 +12,5 @@ LEARNER::base_learner* cb_explore_adf_setup(arguments& arg);
 
 namespace CB_EXPLORE_ADF
 {
-  example* test_adf_sequence(std::vector<example*>& ec_seq);
+  example* test_adf_sequence(v_array<example*>& ec_seq);
 }
--- vowpalwabbit/parser.cc
+++ vowpalwabbit/parser.cc
@@ -983,7 +983,7 @@ void finish_example(vw& all, multi_ex& ec_seq)
 {
   for(auto ec : ec_seq)
     finish_example(all, *ec);
-  ec_seq.clear();
+  ec_seq.erase();
 }
 
 void finish_example(vw& all, example& ec)
